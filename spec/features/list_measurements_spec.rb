fc45454c8e9473c1b8352dab2480c639f486cef8
Robert Curth <robert@rocu.de>
2013-09-08 10:24:03 +0200

    Showing a hint if no recent measurements exist

--- spec/features/list_measurements_spec.rb
+++ spec/features/list_measurements_spec.rb
@@ -1,11 +1,19 @@
 require 'spec_helper'
 
 feature 'View Measurements' do
+  scenario 'Viewing a list without measurements' do
+    Given 'No recent measurements'
+    When 'I view the list of recent measurements'
+    Then 'I see a message that no recent measurements were taken'
+  end
+  
   scenario 'Viewing a list of recent measurements' do
     Given 'Recent measurements'
     When 'I view the list of recent measurements'
     Then 'I see a list of recent measurements'
   end
+
+  def no_recent_measurements; end
 
   def recent_measurements
     @measurements = []
@@ -22,5 +30,9 @@ feature 'View Measurements' do
       expect(page).to have_content(m.hot_water)
       expect(page).to have_content(m.cold_water)
     end
+  end
+
+  def i_see_a_message_that_no_recent_measurements_were_taken
+    expect(page).to have_content('No recent measurements')
   end
 end
